b222e771f554ba0ccc12a61c868964e53c82d972
devjanaprime <dev@primeacademy.io>
2016-09-13 20:26:17 -0500

    updated button ids and added basic object append functionality

--- scripts/jqueryDom.js
+++ scripts/jqueryDom.js
@@ -5,16 +5,6 @@ $( document ).ready( function(){
 
   $( '#showObject' ).on( 'click', function(){
     console.log( 'showObject on click' );
-    // display exampleObject
-    // store our outputDiv in a variable
-    var theDiv = $( '#outputDiv' );
-    // empty the outputDiv
-    theDiv.empty();
-    // create a p tag that will hold our object's name:
-    var outputText = '<p>' + exampleObject.name + '</p>';
-    // append this p tag to the div
-    theDiv.append( outputText );
-    // next step... add p tag that displays exampleObject.status?
   }); // end showObject on click
 
   $( '#showObjectArray' ).on( 'click', function(){
