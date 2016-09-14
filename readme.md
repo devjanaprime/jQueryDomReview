8cd1209d764a289271abc653284203e27057af6c
devjanaprime <dev@primeacademy.io>
2016-09-13 20:24:08 -0500

    updated button ids and added basic object append functionality

--- readme.md
+++ readme.md
@@ -10,3 +10,5 @@ Objective: Review putting things on the DOM using JQuery
 --------------------------------------------------------
 * Objects
 * Array of Objects
+
+We'll have a div on the DOM with an id of "outputDiv". We will be appending info to the DOM in this div. There are two buttons on the DOM that append to the DOM. One will append an object to the DOM. The other will loop through an array of Objects and append them all to this div.
--- scripts/jqueryDom.js
+++ scripts/jqueryDom.js
@@ -5,9 +5,19 @@ $( document ).ready( function(){
 
   $( '#showObject' ).on( 'click', function(){
     console.log( 'showObject on click' );
+    // display exampleObject
+    // store our outputDiv in a variable
+    var theDiv = $( '#outputDiv' );
+    // empty the outputDiv
+    theDiv.empty();
+    // create a p tag that will hold our object's name:
+    var outputText = '<p>' + exampleObject.name + '</p>';
+    // append this p tag to the div
+    theDiv.append( outputText );
+    // next step... add p tag that displays exampleObject.status?
   }); // end showObject on click
 
-  $( '#showArray' ).on( 'click', function(){
+  $( '#showObjectArray' ).on( 'click', function(){
     console.log( 'showArray on click' );
   }); // end showArray on click
 });
@@ -18,7 +28,7 @@ var exampleObject = {
   status: 'lonely'
 }; // end exampleObject
 
-// objects for an array
+// array of objects
 var jetsons = [
 { name: 'George', status: 'dad' },
 { name: 'Jane', status: 'mom' },
